31759073045c22929eb50ad7fb5195a5514fac59
isral Duke <isralduke@gmail.com>
2026-07-25 15:13:38 -0500

    CSS Tweaks

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,6 +5,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("_input/assets");
   eleventyConfig.addPassthroughCopy("_input/images");
 
+  eleventyConfig.addCollection("product-design", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("_input/product-design/*.md").filter(
+      (item) => item.inputPath !== "./_input/product-design/index.md"
+    );
+  });
+
   return {
     markdownTemplateEngine: "njk",
     dir: {
